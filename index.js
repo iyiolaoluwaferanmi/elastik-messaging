af735df44b2fbe1c9cb476fa1e0195f928860270
Iyiola Oluwaferanmi <gossipears@gmail.com>
2025-04-28 16:58:43 +0400

    Updated logic
    updated display logic

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var xhub = require('express-x-hub');
 
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port ${app.get('port')}`);
+});
 
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
 var token = process.env.TOKEN || 'token';
-var received_updates = [];
+
+// This stores all messages grouped by WA ID
+const customerMessages = {};
 
 app.get('/', function(req, res) {
   console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send('<pre>' + JSON.stringify(customerMessages, null, 2) + '</pre>');
 });
 
 app.get(['/facebook', '/instagram', '/threads'], function(req, res) {
@@ -35,38 +31,6 @@ app.get(['/facebook', '/instagram', '/threads'], function(req, res) {
     res.sendStatus(400);
   }
 });
-
-/* app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
-
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
-
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-}); */
-
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/threads', function(req, res) {
-  console.log('Threads request body:');
-  console.log(req.body);
-  // Process the Threads updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', JSON.stringify(req.body, null, 2));
@@ -110,5 +74,3 @@ function storeMessage(wa_id, timestamp, textBody) {
     message: textBody
   };
 }
-
-app.listen();
